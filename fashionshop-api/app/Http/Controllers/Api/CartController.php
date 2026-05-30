e0d86ac85ccfcd94d8f05e18a7dcbe495ca516ca
Phuongvy260905 <vynnp6064@ut.edu.vn>
2026-05-31 00:13:09 +0700

    FAS-6 fix: size không hợp lệ

--- fashionshop-api/app/Http/Controllers/Api/CartController.php
+++ fashionshop-api/app/Http/Controllers/Api/CartController.php
@@ -19,12 +19,28 @@ class CartController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'product_id' => 'required|exists:products,id',
-            'quantity'   => 'required|integer|min:1',
-            'size'       => 'required|in:S,M,L,XL',
-        ]);
+        $validator = \Validator::make($request->all(), [
+    'product_id' => 'required|exists:products,id',
+    'quantity'   => 'required|integer|min:1',
+    'size'       => 'required|in:S,M,L,XL',
+]);
 
+if ($validator->fails()) {
+
+    if ($validator->errors()->has('size')) {
+        return response()->json([
+            'message' => 'Size không hợp lệ',
+            'errors' => [
+                'size' => $validator->errors()->get('size')
+            ]
+        ], 200);
+    }
+
+    return response()->json([
+        'message' => 'Dữ liệu không hợp lệ',
+        'errors' => $validator->errors()
+    ], 200);
+}
         $cart = Cart::where('user_id', $request->user()->id)
             ->where('product_id', $request->product_id)
             ->where('size', $request->size)
